1256fc8f961f17de444619735ae9fe412efb59cf
Christian Urbanek <curbanek01@gmail.com>
2023-11-13 04:15:02 -0600

    new version

--- Bidding/iteration3_bid.py
+++ Bidding/iteration3_bid.py
@@ -7,10 +7,10 @@ load_dotenv()
 
 app = Flask(__name__)
 
-client = MongoClient("mongodb://localhost:27017/bid_db")
-db = client.your_database_name
+client = MongoClient("mongodb://localhost:27017/")
+db = client['bid_db']
 
-bid_collection = db.bids
+bid_collection = db['bids']
 
 example_bid = {
     'seller': 'Mark',
@@ -18,13 +18,19 @@ example_bid = {
     'price': '$50'
 }
 
-bid_collection.insert_one(example_bid)
+x = bid_collection.insert_one(example_bid)
+
+print("test")
+print(x)
+print(client.list_database_names())
+print(db.list_collection_names())
+print(bid_collection.find())
 
 @app.route('/bids', methods=['GET'])
 def get_bids():
-    bookings = bid_collection.find()
-    booking_list = [{'id': str(booking['_id']), 'seller': booking['seller'], 'bidder': booking['bidder'], 'price': booking['price']} for booking in bookings]
-    return jsonify(booking_list)
+    bids = bid_collection.find()
+    bid_list = [{'id': str(bid['_id']), 'seller': bid['seller'], 'bidder': bid['bidder'], 'price': bid['price']} for bid in bids]
+    return jsonify(bid_list)
 
 @app.route('/bids', methods=['POST'])
 def create_bid():
