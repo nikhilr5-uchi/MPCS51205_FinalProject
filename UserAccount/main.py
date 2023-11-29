a6cdf31b9795ffd4feb38f6ee68d72a35fe85067
Christian Urbanek <curbanek01@gmail.com>
2023-11-28 23:21:20 -0600

    Merged with database calls

--- UserAccount/main.py
+++ UserAccount/main.py
@@ -2,6 +2,13 @@ from flask import Blueprint, render_template, redirect, url_for, request
 # from . import db
 import pymongo
 
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client['bid_db']
+
+bid_collection = db['bids']
+
 main = Blueprint('main', __name__) 
 
 # Sample user data
@@ -120,7 +127,8 @@ def product_details(listing_id):
 def my_listings():
     return render_template('my_listings.html', user=sample_user)
 
-@main.route('/place_bid/<int:listing_id>', methods=['POST'])
+#THIS IS THE ORIGINAL place_bid funtion without database changes
+'''@main.route('/place_bid/<int:listing_id>', methods=['POST'])
 def place_bid(listing_id):
     # Fetch the listing from the user's data
     listing = next((listing for listing in sample_user['listings'] if listing['id'] == listing_id), None)
@@ -143,7 +151,64 @@ def place_bid(listing_id):
         bid_status = 'danger'
 
     # Pass bid-related messages to the template
+    return render_template('product_details.html', listing=listing, bid_message=bid_message, bid_status=bid_status)'''
+
+#THIS IS THE NEW place_bid function where bids also get stored in databases
+@main.route('/place_bid/<int:listing_id>', methods=['POST'])
+def place_bid(listing_id):
+    # Fetch the listing from the user's data
+    listing = next((listing for listing in sample_user['listings'] if listing['id'] == listing_id), None)
+
+    if listing:
+        # Get the current bid amount from the form
+        bid_amount = float(request.form.get('bid_amount', 0.0))
+
+        # Ensure the bid amount is greater than the current bid
+        if bid_amount > listing['min_bid']:
+            # Update the minimum bid for the listing
+            listing['min_bid'] = bid_amount
+            bid_collection.insert_one({"bid_amount" : bid_amount})
+            bid_message = 'Bid placed successfully!'
+            bid_status = 'success'
+        else:
+            bid_message = 'Bid must be greater than the current minimum bid.'
+            bid_status = 'danger'
+    else:
+        bid_message = 'Listing not found.'
+        bid_status = 'danger'
+
+    # Pass bid-related messages to the template
     return render_template('product_details.html', listing=listing, bid_message=bid_message, bid_status=bid_status)
+
+#Get all the bids in the database and return as a list
+@main.route('/get_bids/<int:listing_id>', methods=['GET'])
+def get_bids():
+    bids = bid_collection.find()
+    bid_list = [{'id': str(bid['_id']), 'bid_amount': bid['bid_amount']} for bid in bids]
+    return bid_list
+
+#Use largest bid in database and inrement_amount to get next bid increment
+@main.route('/increment_bid/<int:listing_id>', methods=['PUT'])
+def increment_bid(increment_amount):
+
+    # Update the bid amount for the next bid by incrementing it
+    pipeline = [
+    {"$group": {"_id": None, "maxBidAmount": {"$max": "$bid_amount"}}},
+    {"$project": {"_id": 0, "maxBidAmount": 1}}
+    ]
+
+    current_bids = list(bid_collection.aggregate(pipeline))
+
+    # Check if there is a result and retrieve the maximum bid amount
+    if current_bids:
+        max_bid_amount = current_bids[0]['maxBidAmount']
+        print("Next Bid Amount:", (max_bid_amount + increment_amount))
+    else:
+        print("No data found in the collection.")
+
+    new_bid_amount = max_bid_amount + increment_amount
+
+    return new_bid_amount
 
 @main.route('/delete_listing/<int:listing_id>', methods=['POST'])
 def delete_listing(listing_id):
@@ -178,4 +243,3 @@ def process_checkout(listing_id):
     else:
         # Handle listing not found
         return redirect(url_for('main.shopping_cart'))
-
